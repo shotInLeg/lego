4abb48aa5e7fab95fb983b96b9b466c8e6867e89
Anton Zvonarev <shotinleg@yandex.ru>
2017-10-19 00:14:11 +0300

    Обновлены Unit-тесты для лексера

--- test/test_lexer/test_lexer.py
+++ test/test_lexer/test_lexer.py
@@ -1,5 +1,9 @@
+import os
+
 from unittest import TestCase
 from legoc.lexer.settings import regexp
+from legoc.lexer.lexer import Lexer
+from legoc.lexer.types import *
 
 
 class TestLexer(TestCase):
@@ -17,6 +21,7 @@ class TestLexer(TestCase):
             '+', '+=',  # loperation
             'if',  # loperator
             'A', 'AbAc', 'Ab(Ac, Ad)', 'Ab<Bc>',  # ltype
+            '(', ')', '{', '}', '[', ']', '<', '>',  # lbracket
             '1', '12', '1_000', '12.14', '"qwerty"'  # lvalue
         ]
 
@@ -26,6 +31,8 @@ class TestLexer(TestCase):
             'loperation', 'loperation',
             'loperator',
             'ltype', 'ltype', 'ltype', 'ltype',
+            'lopen_bracket', 'lclose_bracket', 'lopen_bracket', 'lclose_bracket',
+            'lopen_bracket', 'lclose_bracket', 'loperation', 'loperation',
             'lvalue', 'lvalue', 'lvalue', 'lvalue', 'lvalue'
         ]
 
@@ -38,4 +45,42 @@ class TestLexer(TestCase):
                     item_matches.append(regexp[reg])
                     current[i] = max(item_matches)[0]
 
-        self.assertEquals(actual, current)
+        self.assertEqual(actual, current)
+
+    def test_lexer_run(self):
+        actual = {
+            os.path.join(
+                os.path.dirname(__file__),
+                'lexer_run_test_files',
+                'test_spaces.txt'
+            ): [
+                LNameType('a'), LOperationType('+'), LValueType('5'),
+                LNameType('b'), LOperationType('+'), LValueType('6')
+            ],
+
+            os.path.join(
+                os.path.dirname(__file__),
+                'lexer_run_test_files',
+                'test_brackets.txt'
+            ): [
+                LOpenBracketType('('), LCloseBracketType(')'),
+                LOpenBracketType('{'), LCloseBracketType('}'),
+                LOpenBracketType('['), LCloseBracketType(']'),
+                LOperationType('<'),LOperationType('>'),
+                LOpenBracketType('('), LCloseBracketType(')'),
+                LOpenBracketType('{'), LCloseBracketType('}'),
+                LOpenBracketType('['), LCloseBracketType(']'),
+                LOperationType('<'), LOperationType('>'),
+                LOpenBracketType('('), LCloseBracketType(')'),
+                LOpenBracketType('{'), LCloseBracketType('}'),
+                LOpenBracketType('['), LCloseBracketType(']'),
+                LOperationType('<'), LOperationType('>'),
+            ]
+        }
+
+        lexer = Lexer()
+        for test_file in actual:
+            tokens = lexer.run(test_file)
+            self.assertEqual(len(actual[test_file]), len(tokens), 'Invalid size')
+            for act, cur in zip(actual[test_file], tokens):
+                self.assertEqual(act, cur, '{} != {}'.format(act, cur))
