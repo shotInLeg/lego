7aa23a55c2ba0b6c597fb2dbdbdbcde16a7dbbe4
Anton Zvonarev <shotinleg@yandex.ru>
2017-12-01 19:38:57 +0300

    Исправлены ошибки

--- legoc/model/types/moperator/mif.py
+++ legoc/model/types/moperator/mif.py
@@ -1,12 +1,12 @@
 from legoc.model.types.moperator.moperator import MOperator
 
 
-class MWhile(MOperator):
+class MIf(MOperator):
     def __init__(self, if_mexp, if_lst_instr,
                  lst_elif_mexp, lst_elif_lst_instr,
                  else_lst_instr):
-        super(MWhile, self).__init__()
-        self.tstack.append(MWhile.__name__)
+        super(MIf, self).__init__()
+        self.tstack.append(MIf.__name__)
 
         self.if_cond = if_mexp
         self.if_instructions = if_lst_instr
